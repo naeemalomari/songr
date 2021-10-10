b87f22e01222d5651c901fd8294fe911538c3dee
naeemalomari <naeemalomari96@gmail.com>
2021-10-10 23:46:09 +0300

    last commit wallah

--- src/test/java/com/asac/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/asac/helloworld/HelloworldApplicationTests.java
@@ -1,13 +1,26 @@
 package com.asac.helloworld;
 
+import com.asac.helloworld.model.Album;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class HelloworldApplicationTests {
+class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testConstructor() {
+		Album album = new Album("Title","Artist",10,120,"https://editorial.uefa.com/resources/026c-12f705c46a6c-9f2eb0579483-1000/messi_graphic.jpg");
+		album.setTitle("Title");
+		assertEquals("Title",album.getTitle());
+		album.setArtist("Artist");
+		assertEquals("Artist",album.getArtist());
+		album.setSongCount(20);
+		assertEquals(20,album.getSongCount());
+		album.setLength(10);
+		assertEquals(10 , album.getLength());
 	}
 
 }
+
