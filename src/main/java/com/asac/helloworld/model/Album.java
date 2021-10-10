66d982fb8d87f50f960efa233c62a22c4ca0998e
naeemalomari <naeemalomari96@gmail.com>
2021-10-10 23:23:46 +0300

    lab11

--- src/main/java/com/asac/helloworld/model/Album.java
+++ src/main/java/com/asac/helloworld/model/Album.java
@@ -11,11 +11,11 @@ import java.util.List;
 public class Album {
 
 
-private String title;
-private String artist;
-private int songCount;
-private int length;
-private String imageUrl;
+ String title;
+ String artist;
+ int songCount;
+ int length;
+ String imageUrl;
 //private ArrayList<String> albums;
 
     public Album(String title, String artist, int songCount, int length, String imageUrl) {
